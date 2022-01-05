9639286857d2787ba8b8399b36adc024438aeb94
Sergiy Nimerovsky <sergiynimerovsky@Sergiys-MacBook-Pro.local>
2022-01-05 20:34:48 +0200

    build: add welcome text

--- src/telegram_bot/crypto/keyboards.js
+++ src/telegram_bot/crypto/keyboards.js
@@ -1,3 +1,8 @@
-const {Keyboard, Key} = require('telegram-keyboard')
+import {BOT_BUTTONS} from "../../utils/messages";
+const {Keyboard} = require('telegram-keyboard')
 
-
+export const welcomeKeyboard = () => {
+    return Keyboard.make([
+        [BOT_BUTTONS['deposit']],
+    ])
+}
--- src/telegram_bot/crypto/services.js
+++ src/telegram_bot/crypto/services.js
@@ -1,3 +1,6 @@
+import {welcomeKeyboard} from "./keyboards";
+import {BOT_MESSAGES} from "../../utils/messages";
+
 export default class Services {
     constructor(app) {
         this.app = app;
@@ -5,10 +8,8 @@ export default class Services {
 
     replyWelcome = async (ctx) => {
         try {
-            return ctx.reply(`👋 Привіт
-
-📝 Сюди можна ввести свій місячний депозит і я вирухую скільки потрібно інвестувати в кожну криптовалюту кожної неділі`,
-                Extra.HTML())
+            return ctx.reply(BOT_MESSAGES['welcome'],
+                welcomeKeyboard().reply())
         } catch (e) {
             this.app.logger.error(e.message, {
                 function: 'replyWelcome'
