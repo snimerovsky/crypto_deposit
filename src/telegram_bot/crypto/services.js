eb2a37690c6929882309f8efe3abb1fff08742d8
Sergiy Nimerovsky <sergii.nimerovsky@gmail.com>
2022-03-06 13:04:32 +0200

    build: add photo

--- src/telegram_bot/crypto/services.js
+++ src/telegram_bot/crypto/services.js
@@ -46,14 +46,58 @@ module.exports = class Services {
             const notion_data = await this.getNotionCryptoData()
             const market_cap = await this.getMarketCap(notion_data)
 
-            console.log('market_cap', market_cap)
+            const image = await chartJSNodeCanvas.renderToBuffer(this.getDoughnutConfig(notion_data));
+            const image2 = await chartJSNodeCanvas.renderToBuffer(this.getBarConfig(market_cap));
 
             const text = this.calculateDeposit(notion_data, sum, moment().format('YYYY-MM-DD'))
 
-            return ctx.reply(text, depositKeyboard().reply({
-                parse_mode: 'HTML',
-                disable_web_page_preview: true
-            }))
+            const date = new Date().getTime()
+
+            fs.writeFile(`image1${date}.png`, image, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+
+                fs.writeFile(`image2${date}.png`, image2, (err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+
+                    joinImages([`image1${date}.png`, `image2${date}.png`], {
+                        direction: 'horizontal'
+                    }).then(async (img) => {
+                        await img.toFile(`out${date}.png`);
+
+                        fs.readFile(`out${date}.png`, async (err, data) => {
+                            if (err) {
+                                console.log(err)
+                            }
+
+                            await ctx.replyWithPhoto({source: `out${date}.png`}, depositKeyboard().reply({
+                                caption: text,
+                                parse_mode: 'HTML',
+                                disable_web_page_preview: true
+                            }))
+
+                            fs.unlink(`image1${date}.png`, (err) => {
+                                if (err) {
+                                    console.log(err)
+                                }
+                            })
+                            fs.unlink(`image2${date}.png`, (err) => {
+                                if (err) {
+                                    console.log(err)
+                                }
+                            })
+                            fs.unlink(`out${date}.png`, (err) => {
+                                if (err) {
+                                    console.log(err)
+                                }
+                            })
+                        })
+                    });
+                })
+            })
         }
         catch (e) {
             await ctx.reply(`Виникла помилка: ${e.message}`)
@@ -63,12 +107,21 @@ module.exports = class Services {
 
     getMarketCap = async (notion_data) => {
         const currencies_res = await axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.CRYPTO_TOKEN}&ids=${notion_data.map(v => v['symbol']).join(',')}&per-page=100&page=1`)
-        const market_cap = {}
+        const market_cap_res = {}
+
         currencies_res['data'].forEach(v => {
-            market_cap[v['id']] = v['market_cap']
+            market_cap_res[v['id']] = v['market_cap']
         })
 
-        return market_cap
+        notion_data.forEach(v => {
+            const market_cap = market_cap_res[v['symbol']]
+            market_cap_res[v['symbol']] = {
+                ...v,
+                market_cap
+            }
+        })
+
+        return market_cap_res
     }
 
     calculateDeposit = (data, sum, date) => {
@@ -129,5 +182,60 @@ ${data.map(currency_data => {
         date.date(1);
         const dif = (7 + (weekday - date.weekday()))%7+1;
         return Math.floor((date.daysInMonth()-dif) / 7)+1;
+    }
+
+    getDoughnutConfig = (notion_data) => {
+        return {
+            type: 'doughnut',
+            data: {
+                labels: notion_data.map(v => v['title']),
+                datasets: [{
+                    label: '% в портфелі',
+                    data: notion_data.map(v => +v['profile_percent'] * 100),
+                    backgroundColor: notion_data.map(v => `rgb(${v['color']})`),
+                    hoverOffset: 4,
+                }]
+            },
+            options: {
+                showAllTooltips: true,
+                plugins: {
+                    datalabels: {
+                        color: '#fff',
+                        font: {
+                            size: 20,
+                            weight: 'bold'
+                        },
+                        formatter: function(value, context) {
+                            return value + '%';
+                        }
+                    },
+                }
+            },
+        }
+    }
+
+    getBarConfig = (market_cap) => {
+        return {
+            type: 'bar',
+            data: {
+                labels: Object.values(market_cap).map(v => v['title']),
+                datasets: [{
+                    label: 'Ринкова капіталізація',
+                    data: Object.values(market_cap).map(v => v['market_cap']),
+                    backgroundColor: Object.values(market_cap).map(v => `rgba(${v['color']}, 0.2)`),
+                    borderColor: Object.values(market_cap).map(v => `rgb(${v['color']})`),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                plugins: {
+                    datalabels: {
+                        formatter: function(value, context) {
+                            return '';
+                        }
+                    },
+                }
+            },
+        }
     }
 }
